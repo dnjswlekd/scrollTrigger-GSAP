7c97b8b00ecefe4427bedab2b67c686fb9c7863d
dnjswlekd <wj8014@gmail.com>
2025-03-02 22:10:36 +0900

    scroll trigger section2

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $(function () {
         start: 'top 90%',
         end: '20% 100%',
         scrub: 2,
-        markers: true,
+        markers: false,
       },
     })
     .to(
@@ -31,4 +31,21 @@ $(function () {
       { y: '-400px', duration: 1, ease: 'none' },
       0.8
     );
+
+  // content03
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: '.cont03',
+        start: 'top 100%',
+        end: '100% 100%',
+        scrub: 2,
+        markers: true,
+      },
+    })
+    .fromTo('.cont03 .a', { y: '400%' }, { y: '0' }, 1.2)
+    .fromTo('.cont03 .b', { y: '400%' }, { y: '0' }, 1.4)
+    .fromTo('.cont03 .c', { y: '400%' }, { y: '0' }, 1.6)
+    .fromTo('.cont03 .d', { y: '400%' }, { y: '0' }, 1.8)
+    .fromTo('.cont03 .e', { y: '400%' }, { y: '0' }, 2);
 });
